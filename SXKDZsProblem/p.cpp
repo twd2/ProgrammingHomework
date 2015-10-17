8406c634ee6017d37d3674794cf6fcdb0b85406a
Wende Tan <twd2@163.com>
2015-10-17 14:38:36 +0800

    bugfix

--- SXKDZsProblem/p.cpp
+++ SXKDZsProblem/p.cpp
@@ -27,100 +27,110 @@ vector<string> split(const string &s, const string &delim)
 
 int indexOf(vector<string> &a, string &s)
 {
-    int size=a.size();
-    for (int i=0;i<size;++i)
+    int size = a.size();
+    for (int i = 0; i < size; ++i)
     {
-        if(a[i]==s) return i;
+        if (a[i] == s) return i;
     }
     return -1;
 }
 
 bool hasIntersection(vector<string> &a, vector<string> &b)
 {
-    for(vector<string>::iterator it=a.begin();it!=a.end();++it)
+    for (vector<string>::iterator it = a.begin(); it != a.end(); ++it)
     {
-        if(indexOf(b, *it)>=0) return true;
+        if (indexOf(b, *it) >= 0) return true;
     }
     return false;
 }
 
 void combine(vector<string> &a, vector<string> &b)
 {
-    for(vector<string>::iterator it=b.begin();it!=b.end();++it)
+    for (vector<string>::iterator it = b.begin(); it != b.end(); ++it)
     {
-        if(indexOf(a, *it)<0) a.push_back(*it);
+        if (indexOf(a, *it) < 0) a.push_back(*it);
     }
     b.clear();
 }
 
 bool myComp(const vector<string> &a, const vector<string> &b)
 {
-    return !(a.size()<b.size());
+    return !(a.size() < b.size());
 }
 
 int main()
 {
     vector< vector<string> > friendCircle;
     
-    while (true) {
+    while (true)
+    {
 
 		string nameLine;
 		getline(cin, nameLine);
-		if (nameLine.size()==0 || cin.eof()) {
+		if (nameLine.size() == 0 || cin.eof())
+		{
 			break;
 		}
 
 		nameLine.erase(0, 1);
 		nameLine.erase(nameLine.size() - 1, 1);
 
-		vector<string> friendName = split(nameLine, string(" "));
+        if (nameLine.size() == 0)
+		{
+			continue;
+		}
 
-        friendCircle.push_back(friendName);
+		vector<string> friendName = split(nameLine, string(" "));
+    
+        if (friendName.size() > 0)
+        {
+            friendCircle.push_back(friendName);
+        }
 	}
 	
-	int size=friendCircle.size();	
+	int size = friendCircle.size();	
 	
-	bool found=false;
+	bool found = false;
 	do
 	{
-        found=false;
-        for(int i=0;i<size;++i)
+        found = false;
+        for (int i = 0; i < size; ++i)
         {
-            for(int j=0;j<size;++j)
+            for (int j = 0; j < size; ++j)
             {
-                if(i==j) continue;
+                if (i == j) continue;
                 
-                vector<string> &a=friendCircle[i],
-                               &b=friendCircle[j];
-                if(hasIntersection(a, b)) //对于任意两个不同的集合，判断是否有交集
+                vector<string> &a = friendCircle[i],
+                               &b = friendCircle[j];
+                if (hasIntersection(a, b)) //对于任意两个不同的集合，判断是否有交集
                 {
-                    found=true;
+                    found = true;
                     combine(a, b); //有就合并
                 }
             }
         }
-	} while(found);
+	} while (found);
 	
-	for(int i=0;i<size;++i)
+	for (int i = 0; i < size; ++i)
 	{
-        vector<string> &a=friendCircle[i];
+        vector<string> &a = friendCircle[i];
         sort(a.begin(), a.end());
 	}
 	
 	sort(friendCircle.begin(), friendCircle.end(), myComp);
 	
-    for(int i=0;i<size;++i)
+    for (int i = 0; i < size; ++i)
 	{
-        vector<string> &a=friendCircle[i];
-        if(a.size()==0) break;
-        int nameCount=a.size();
-        cout<<"{";
-        for(int j=0;j<nameCount;++j)
+        vector<string> &a = friendCircle[i];
+        if (a.size() == 0) break;
+        int nameCount = a.size();
+        cout << "{";
+        for (int j = 0; j < nameCount; ++j)
         {
-            cout<<a[j];
-            if(j<nameCount-1) cout<<" ";
+            cout << a[j];
+            if (j < nameCount - 1) cout << " ";
         }
-        cout<<"}"<<endl;
+        cout << "}" << endl;
 	}
     return 0;
 }
