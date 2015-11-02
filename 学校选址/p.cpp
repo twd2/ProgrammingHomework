4288ddcc58b7dd7bfc391feacd8396b58988d1bf
twd2 <twd2@163.com>
2015-11-02 21:08:22 +0800

    AC!

--- 学校选址/p.cpp
+++ 学校选址/p.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	int n, a[10050], b[10050], maxX = 0;
-	long long minD = 10000L * 1000000 * 10;
-	int minX = -1;
+	int n, a[10050], b[10050], ab[100050], count = 0;
+
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> a[i];
-		if (a[i] > maxX) maxX = a[i];
 	}
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> b[i];
+		for (int j = 0; j < b[i]; ++j)
+		{
+			ab[count++] = a[i];
+		}
 	}
 	
-	for (int x = 0; x <= maxX; ++x)
-	{
-		long long D = 0;
-		for (int i = 0; i < n; ++i)
-		{
-			D += abs(a[i] - x) * b[i];
-		}
-		//cout << D << endl;
-		if (D < minD) 
-		{
-			minD = D;
-			minX = x;
-		}
-	}
+	sort(ab, ab + count);
+	
+	int minX = ab[count / 2];
 	
 	const int mod = 10007;
+	long long longD = 0;
 	
-	cout << minX << " " << minD % 10007 << endl;
+	for (int i = 0; i < n; ++i)
+	{
+		longD += ((abs(a[i] - minX) % mod) * (b[i] % mod)) % mod;
+		longD %= mod;
+	}
+	cout << minX << " " << longD << endl;
 	return 0;
 }
