68d2dcdecfda4f89810e11587fda7224ef4e56b8
Ben Pig Chu <benpichu@gmail.com>
2015-11-29 15:18:56 +0800

    AC with cout is possible!

---
+++ test2/正整数拆分/passwithcout.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+struct task{
+	int sum,last;
+	vector<int> list;
+	task *next;
+};
+ 
+task *head=NULL;
+ 
+void push(task x){
+	task *temp;
+	temp=new task;
+	*temp=x;
+	temp->next=head;
+	head=temp;
+}
+ 
+task pop(){
+	task *temp,t;
+	temp=head;
+	head=temp->next;
+	t=*temp;
+	t.next=NULL;
+	delete temp;
+	return t;
+}
+ 
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		task temp;
+		temp.next=NULL;
+		temp.list.push_back(i);
+		temp.sum=i;
+		temp.last=i;
+		push(temp);
+	}
+	while(head!=NULL){
+		task t;
+		t=pop();
+		if(t.sum==n){
+			for(vector<int>::iterator it=t.list.begin();it!=t.list.end();it++){
+				if(it!=t.list.begin())cout<<" ";
+				cout<<*it; 
+			}
+			cout<<endl;
+		}else{
+			int end;
+			end=(n-t.sum)>t.last?t.last:(n-t.sum);
+			for(int i=1;i<=end;i++){
+				task temp;
+				temp.next=NULL;
+				temp.list=t.list;
+				temp.list.push_back(i);
+				temp.sum=t.sum+i;
+				temp.last=i;
+				push(temp);
+			}
+		}
+	}
+}
