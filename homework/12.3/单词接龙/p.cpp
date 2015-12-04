0aafc1d40941900648ee5a72a961bd26cf24117f
Wende Tan <twd2@163.com>
2015-12-05 02:57:27 +0800

    wtf bugfix

--- homework/12.3/单词接龙/p.cpp
+++ homework/12.3/单词接龙/p.cpp
@@ -1,17 +1,24 @@
+#define FILE_IO
+
+#ifndef FILE_IO
 #include <iostream>
+#else
 #include <fstream>
+#endif 
+
 #include <string>
+#include <cstring>
 using namespace std;
 
-string str[50];
-int used[50] = {0};
-int same[50][50] ={{0}};
 int n;
-int maxDragonLength = 0;
+string str[50];
+int same[50][50] = {{0}};
+int cache[666666]; //>0: value, -1: no value, -2: cannot reach
+int threePow[] = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147};
 
 int sameLength(string a, string b)
 {
-	int maxSame = 0;
+	int result = 0;
 	for (int i = a.length() - 1; i > 0; --i)
 	{
 		bool same = true;
@@ -23,53 +30,94 @@ int sameLength(string a, string b)
 				break;
 			}
 		}
-		if (same) maxSame = a.length() - i;
+		if (same) 
+		{
+			result =  a.length() - i;
+			break; //we need a smaller result to make the dragon longer
+		}
 	}
-	return maxSame;
+	if (result == a.length() || result == b.length()) result = 0;
+	return result;
 }
 
-void foo(int lastStrIndex, int dragonLength)
+int getId(int *used, int lastStrIndex)
 {
-	//cout << dragonLength << " " << lastStrIndex << endl;
-	bool found = false;
+	int id = 0;
 	for (int i = 0; i < n; ++i)
 	{
-		if (used[i] < 2 &&
-			same[lastStrIndex][i] > 0 &&
-			same[lastStrIndex][i] < str[lastStrIndex].length() &&
-			same[lastStrIndex][i] < str[i].length())
+		id += used[i] * threePow[i];
+	}
+	id += lastStrIndex * threePow[n];
+	return id;
+}
+
+void parseId(int id, int *used, int *outLastStrIndex)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		used[i] = (id / threePow[i]) % 3;
+	}
+	*outLastStrIndex = id / threePow[n];
+}
+
+int search(int id)
+{
+	if (cache[id] != -1) return cache[id];
+	
+	//current state
+	int used[50], lastStrIndex;
+	parseId(id, used, &lastStrIndex);
+	
+	if (used[lastStrIndex] == 0)
+	{
+		//invalid state, cannot reach
+		return cache[id] = -2;
+	}
+	
+	//enum. last state
+	for (int i = 0; i < n; ++i)
+	{
+		if (used[i] > 0 && same[i][lastStrIndex] > 0)
 		{
-			++used[i];
-			foo(i, dragonLength + str[i].length() - same[lastStrIndex][i]);
-			--used[i];
-			found = true;
+			--used[lastStrIndex];
+			int lastid = getId(used, i);
+			++used[lastStrIndex];
+			
+			int lastLength = search(lastid);
+			if (lastLength > 0)
+			{
+				int newLength = lastLength + str[lastStrIndex].length() - same[i][lastStrIndex];
+				if (newLength > cache[id]) cache[id] = newLength;
+			}
 		}
 	}
 	
-	if (!found)
-	{
-		//reach the dragon's tail
-		if (dragonLength > maxDragonLength)
-			maxDragonLength = dragonLength;
-	}
+	//still no value, so cannot reach
+	if (cache[id] == -1) cache[id] = -2;
+	
+	return cache[id];
 }
 
 int main()
 {
-	ifstream fin("dragon.in");
-	ofstream fout("dragon.out");
-	
-	fin >> n;
+	memset(cache, 0xFF, sizeof(cache));	
+
+#ifdef FILE_IO
+	ifstream cin("dragon.in");
+	ofstream cout("dragon.out");
+#endif
+
+	cin >> n;
 	
 	for (int i = 0; i < n; ++i)
 	{
-		fin >> str[i];
+		cin >> str[i];
 	}
 	
 	char start;
-	fin >> start;
+	cin >> start;
 	
-	//cache
+	//same length cache
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < n; ++j)
@@ -78,16 +126,32 @@ int main()
 		}
 	}
 	
+	int used[50] = {0};
+	
 	for (int i = 0; i < n; ++i)
 	{
+		++used[i];
+		int id = getId(used, i);
 		if (str[i][0] == start)
 		{
-			++used[i];
-			foo(i, str[i].length());
-			--used[i];
+			cache[id] = str[i].length();
 		}
+		else
+		{
+			//first letter mismatch, cannot reach
+			cache[id] = -2;
+		}
+		--used[i];
+	}
+
+	int maxDragonLength = 0;
+	//enum. states
+	for (int i = 0; i < n * threePow[n]; ++i)
+	{
+		int f = search(i);
+		if (f > maxDragonLength) maxDragonLength = f;
 	}
 	
-	fout << maxDragonLength << endl;
+	cout << maxDragonLength << endl;
 	return 0;
 }
